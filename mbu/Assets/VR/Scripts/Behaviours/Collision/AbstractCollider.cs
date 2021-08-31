f44213651027ad8d91d9c77ad1cae9f09c3413d1
BenSaero <benedict.saerota@gmail.com>
2021-08-31 15:14:53 +0200

    Fixed namespace

--- mbu/Assets/VR/Scripts/Behaviours/Collision/AbstractCollider.cs
+++ mbu/Assets/VR/Scripts/Behaviours/Collision/AbstractCollider.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class AbstractCollider : MonoBehaviour
+
+namespace VRKL.VR.Behaviour
 {
-    protected abstract void OnTriggerEnter(Collider other);
-    protected abstract void OnTriggerExit(Collider other);
+    public abstract class AbstractCollider : MonoBehaviour
+    {
+        protected abstract void OnTriggerEnter(Collider other);
+        protected abstract void OnTriggerExit(Collider other);
+    }
 }
+
+
--- mbu/Assets/VR/Scripts/Behaviours/Collision/ToggleCollider.cs
+++ mbu/Assets/VR/Scripts/Behaviours/Collision/ToggleCollider.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using VRKL.VR.Behaviour;
 
 public class ToggleCollider : AbstractCollider
 {
