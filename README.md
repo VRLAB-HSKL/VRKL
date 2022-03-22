5aeb6171f455c2e4e3a310050d250fd38eeb4562
MBrill <manfred.brill@hskl.de>
2022-03-22 15:48:55 +0100

    MBVR first commit
    VIUSettings ignorieren, die sollte man bei einer neuen VIU-Version eh
    löschen und neu erstellen.
    
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
    Changes to be committed:
    modified:   .gitignore
    modified:   README.md
    modified:   mbu/ProjectSettings/ProjectVersion.txt
    modified:   mbu/UserSettings/EditorUserSettings.asset
    new file:   mbvr/.gitignore
    new file:   mbvr/Assets/MBU.meta
    new file:   mbvr/Assets/MBVR/Examples.meta
    new file:   mbvr/Assets/MBVR/Examples/0 HelloWorld.meta
    new file:   mbvr/Assets/MBVR/Examples/0 HelloWorld/Scenes.meta
    new file:   mbvr/Assets/MBVR/Examples/0 HelloWorld/Scenes/helloScene.unity
    new file:   mbvr/Assets/MBVR/Examples/0 HelloWorld/Scenes/helloScene.unity.meta
    new file:   mbvr/Assets/MBVR/Scripts.meta
    new file:   mbvr/Assets/MBVR/Scripts/QuitVIUSimulator.cs
    new file:   mbvr/Assets/MBVR/Scripts/QuitVIUSimulator.cs.meta
    new file:   mbvr/Assets/VIUSettings.meta
    new file:   mbvr/Packages/manifest.json
    new file:   mbvr/Packages/packages-lock.json
    new file:   mbvr/README.md
    new file:   mbvr/UserSettings/EditorUserSettings.asset

--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 
 In diesem Repository sind Assets und C#-Klassen zusammengefasst,
 die wir bei der Entwicklung von Unity Anwendungen einsetzen können.
+
+Die Assets sind getrennt je nachdem ob sie eine VR-Umgebung wie VIU voraussetzen oder auch in Desktop-Anwendungen eingesetzt werden können.
+
+## MBU
+
 Die Assets setzen keine VR- oder AR-Umgebung voraus, können also auch
 für WebGL- , Desktop- oder Smartphone-Anwendungen eingesetzt werden.
 
-Verwendete Unity-Version: Unity 2020.3.0f1 LTS
+## MBVR
+
+
+
+Verwendete Unity-Version: Unity 2020.3.31f1 LTS
 
 ![Lizenzlogo](https://licensebuttons.net/l/by-nc-sa/3.0/de/88x31.png)
---
+++ mbvr/Assets/MBVR/Scripts/QuitVIUSimulator.cs
@@ -0,0 +1,44 @@
+﻿//========= 2020 - Copyright Manfred Brill. All rights reserved. ===========
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+/// <summary>
+    /// Den VIU-Simulator mit dem Cancel-Button beenden.
+    /// <remarks>
+    /// Wir verwenden die Klasse Input und die Buttons, die
+    /// im Input-Manager definiert sind. Diese Belegungen erhalten 
+    /// wir mit Edit -> Project Settings -> input Manager.
+    /// 
+    /// Dort werden logische Namen wie Submit, Cancel oder Jump
+    /// und die Buttons dafür definiert. Der Vorteil dieser Methode
+    /// ist, dass wir nicht nur physikalisch vorhandene Tasten, sondern
+    /// auch Joystick-Buttons verwenden können wenn sie vorhanden sind.
+    /// 
+    /// Default ist "Fire3", was im Normalfall auf der Tastatur
+	/// der linken Shift-Taste entspricht. Wir müssen etwas anderes als
+	/// den ESC-Button verwenden, da dieser im Simulator bereits
+	/// für das Pausieren der Anwendung eingesetzt wird.
+    /// </remarks>
+    /// </summary>
+    public class QuitVIUSimulator : MonoBehaviour
+    {
+        /// <summary>
+        /// Die Taste mit dem Input-Manager abfragen.
+        /// </summary>
+        private void Update()
+        {
+            if (VIUSettings.activateSimulatorModule)
+            {
+                
+
+                if (Input.GetButton("Fire3"))
+                {
+                    Debug.Log("Simulator wird beendet!");
+                    Application.Quit();
+#if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+#endif
+                }
+            }
+        }
+    }
