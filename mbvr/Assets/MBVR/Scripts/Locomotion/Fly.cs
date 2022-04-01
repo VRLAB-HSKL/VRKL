5be001009fef59c3013895f855310aba65d156e4
MBrill <manfred.brill@hs-kl.de>
2022-04-01 16:25:41 +0200

    Locomotion in MBUVR
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
    Changes to be committed:
    modified:   mbu/Packages/manifest.json
    modified:   mbu/Packages/packages-lock.json
    modified:   mbu/ProjectSettings/ProjectVersion.txt
    modified:   mbvr/Assets/MBVR/Examples/0 HelloWorld/Scenes/helloScene.unity
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion.meta
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Prefabs.meta
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Prefabs/PolePair.prefab
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Prefabs/PolePair.prefab.meta
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scenes.meta
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scenes/FlyScene.unity
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scenes/FlyScene.unity.meta
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scenes/LeaningScene.unity
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scenes/LeaningScene.unity.meta
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scenes/WalkScene.unity
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scenes/WalkScene.unity.meta
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scripts.meta
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scripts/CreatePoles.cs
    new file:   mbvr/Assets/MBVR/Examples/1 Locomotion/Scripts/CreatePoles.cs.meta
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion.meta
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/ContinousMovement.cs
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/ContinousMovement.cs.meta
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/DifferentialDirection.cs
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/DifferentialDirection.cs.meta
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/Fly.cs
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/Fly.cs.meta
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/HipAndHand.cs
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/HipAndHand.cs.meta
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/SingleDirection.cs
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/SingleDirection.cs.meta
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/SupermanFlying.cs
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/SupermanFlying.cs.meta
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/Walk.cs
    new file:   mbvr/Assets/MBVR/Scripts/Locomotion/Walk.cs.meta
    modified:   mbvr/Assets/MBVR/Scripts/QuitVIUSimulator.cs
    new file:   mbvr/Assets/Resources.meta
    new file:   mbvr/Assets/Resources/BillingMode.json
    new file:   mbvr/Assets/Resources/BillingMode.json.meta
    modified:   mbvr/Packages/manifest.json
    modified:   mbvr/Packages/packages-lock.json
    modified:   mbvr/UserSettings/EditorUserSettings.asset

---
+++ mbvr/Assets/MBVR/Scripts/Locomotion/Fly.cs
@@ -0,0 +1,22 @@
+﻿//========= 2021 - Copyright Manfred Brill. All rights reserved. ===========
+using UnityEngine;
+
+/// <summary>
+/// Fliegen 
+/// 
+/// Als Bewegungsrichtung verwenden wir die Orientierung
+/// des Controllers, der durch die Basisklasse Movement festgelegt ist.
+/// </summary>
+public class Fly : SingleDirection
+{
+
+    /// <summary>
+    /// Wir fragen den Vektor forward des Koordinatensystems des Geräts ab, das wir für die
+    /// Richtungsdefinition verwenden und definieren damit die Bewegungsrichtung.
+    /// </summary>
+    protected override void ComputeMovingDirection()
+    {
+        movingDirection = orientationObject.transform.forward;
+        movingDirection.Normalize();
+    }
+}
---
+++ mbvr/Assets/MBVR/Scripts/Locomotion/Walk.cs
@@ -0,0 +1,30 @@
+﻿//========= 2021 - Copyright Manfred Brill. All rights reserved. ===========
+using UnityEngine;
+
+/// <summary>
+/// Laufen (Walk) mit dem Vive Input Utility
+/// 
+/// Bei Fly können wir eine Richtung verwenden, die
+/// alle drei Weltkoordinatenachsen enthält.
+/// Walk beschränkt die Bewegungsrichtung auf eine Richtung
+/// mit konstantem y-Wert.
+/// 
+/// Wir verwenden den Controller, den wir bereits in der Basisklasse
+/// auswählen können und berechnen die Orthogonalprojektion 
+/// dieser Richtung in die Ebene y=0.
+/// </summary>
+public class Walk : SingleDirection
+{
+    /// <summary>
+    /// Wir fragen den Vektor forward des Koordinatensystems des Geräts ab, das wir für die
+    /// Richtungsdefinition verwenden und definieren damit die Bewegungsrichtung.
+    /// <remarks>
+    /// Wir projizieren diese Richtung in die y=0 Ebene und normieren sie anschließend!
+    /// </remarks>
+    /// </summary>
+    protected override void ComputeMovingDirection()
+    {
+        movingDirection = new Vector3(orientationObject.transform.forward.x, 0.0f, orientationObject.transform.forward.z);
+        movingDirection.Normalize();
+    }
+}
