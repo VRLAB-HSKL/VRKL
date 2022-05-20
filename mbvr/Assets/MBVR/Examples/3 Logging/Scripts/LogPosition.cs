b0dced9879f244f6d9b7ae16ce3a3788816207e9
MBrill <manfred.brill@hskl.de>
2022-05-20 11:43:38 +0200

    Logging für Position von getrackten Objekten
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
    s to be committed:
    dified:   mbvr/Assets/MBVR/Examples/3 Logging/Scenes/meshes.unity
    modified:   mbvr/Assets/MBVR/Examples/3 Logging/Scripts/LogPosition.cs
    renamed:    mbvr/Assets/Resources/VRLoggingConfig.xml -> mbvr/Assets/Resources/Log4NetConfig.xml
    renamed:    mbvr/Assets/Resources/VRLoggingConfig.xml.meta -> mbvr/Assets/Resources/Log4NetConfig.xml.meta
    new file:   mbvr/Assets/StreamingAssets/LogOutput.txt
    new file:   mbvr/Assets/StreamingAssets/LogOutput.txt.meta
    modified:   mbvr/UserSettings/EditorUserSettings.asset

--- mbvr/Assets/MBVR/Examples/3 Logging/Scripts/LogPosition.cs
+++ mbvr/Assets/MBVR/Examples/3 Logging/Scripts/LogPosition.cs
@@ -2,6 +2,12 @@ using log4net;
 using UnityEngine;
 using HTC.UnityPlugin.Vive;
 
+/// <summary>
+/// Logging für die Position eines getrackten Objekts.
+/// </summary>
+/// <remarks>
+///Wir verwenden das Logging-Level INFO in Log4Net!
+/// </remarks>
 public class LogPosition : MonoBehaviour
 {
     [Tooltip("Hmd, Device1=RightHand, Device2=LeftHand")]
@@ -9,14 +15,19 @@ public class LogPosition : MonoBehaviour
 
     private void Update()
     {
+        string info = "";
+        string seperator = ";";
         // Wir können mit dem Aufzählungstyp DeviceRole
         // alles abfragen zu dem es getrackte Daten gibt.
         // Hmd, der Default, ist klar.
         // Die beiden Controller erhalten wir mit Device1 (rechts)
         // und Device 2 (links).
         Vector3 trackerTrace = VivePose.GetPoseEx(trackedObject).pos;
-        Debug.Log(trackerTrace);
-        Debug.Log("---");
+        info = trackerTrace.x.ToString() + seperator + 
+                   trackerTrace.y.ToString() + seperator +
+                   trackerTrace.z.ToString() + seperator +
+                   trackedObject.ToString();
+        Log.Info(info);
     }
     
     /// <summary>
