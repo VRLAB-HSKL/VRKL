11c0698cc9b3f1ec4acd7de18aa11819eeaeced1
MBrill <manfred.brill@hskl.de>
2022-08-19 09:43:13 +0200

    QuitSimulator verwendet Jump, also Pause-Taste

--- mbvr/Assets/MBVR/Scripts/QuitVIUSimulator.cs
+++ mbvr/Assets/MBVR/Scripts/QuitVIUSimulator.cs
@@ -3,57 +3,50 @@ using UnityEngine;
 using HTC.UnityPlugin.Vive;
 
 /// <summary>
-/// Namespace für Unity-Assets in VR-Anwendungen
+/// Den VIU-Simulator auf dem Desktop mit einem Button beenden.
 /// </summary>
-namespace VRKL.MBVR
-{
+/// <remarks>
+/// Ziel dieser Klasse ist insbesondere, ein Build des Simulators, das
+/// Full-Screen läuft sinnvoll ohne Einsatz des Task-Managers zu beenden.
+/// 
+/// Wir verwenden die Klasse Input und die Buttons, die
+/// im Input-Manager definiert sind. Wir verwenden aktuell noch nicht
+/// das neue Input-System.
+/// 
+/// Wir finden logische Namen wie Submit, Cancel, Fire<x>
+/// oder Jump und die Belegung dafür definiert. Der Vorteil dieser Methode
+/// ist, dass wir nicht nur physikalisch vorhandene Tasten, sondern
+/// auch Joystick-Buttons verwenden können falls sie vorhanden sind.
+///
+///  Wir müssen etwas anderes als
+/// den ESC-Button verwenden, da dieser im VIU-Simulator bereits
+/// für das Pausieren der Anwendung eingesetzt wird. Anschließend
+/// kann dann im Inspektor der Play-Button deaktiviert werden.
+/// 
+/// Die Fire<x>-Buttons scheiden aus, da ´die Maustasten in der
+/// Simulatoin der Controller eingesetzt werden.
+ ///
+ /// Deshalb fällt die Entscheidung aktuell für "Jump". Auf dem Keyboard
+ /// entspricht dies der Pause-Taste!
+ /// </remarks>
+public class QuitVIUSimulator : MonoBehaviour
+{ 
     /// <summary>
-    /// Den VIU-Simulator auf dem Desktop mit einem Button beenden.
-    /// <remarks>
-    /// Wir verwenden die Klasse Input und die Buttons, die
-    /// im Input-Manager definiert sind. Diese Belegungen erhalten 
-    /// wir mit Edit -> Project Settings -> input Manager.
-    /// 
-    /// Dort werden logische Namen wie Submit, Cancel oder Fire<x>
-    /// und die Belegung dafür definiert. Der Vorteil dieser Methode
-    /// ist, dass wir nicht nur physikalisch vorhandene Tasten, sondern
-    /// auch Joystick-Buttons verwenden können falls sie vorhanden sind.
-    /// 
-    /// Default ist "Fire2", was im Normalfall auf der Tastatur
-    /// der linken Alt-Taste entspricht. Wir müssen etwas anderes als
-    /// den ESC-Button verwenden, da dieser im VIU-Simulator bereits
-    /// für das Pausieren der Anwendung eingesetzt wird.
-    ///
-    /// "Fire3" scheidet inzwischen aus, da wir die Shift-Taste im Simulator
-    /// für das Touchpad benötigen!
-    /// </remarks>
+    /// Die Taste mit dem Input-Manager abfragen.
     /// </summary>
-    public class QuitVIUSimulator : MonoBehaviour
+    private void Update()
     {
-        /// <summary>
-        /// Die Taste mit dem Input-Manager abfragen.
-        /// </summary>
-        private void Update()
+        if (VIUSettings.activateSimulatorModule && Input.GetButton(STOP_BUTTON))
         {
-            if (VIUSettings.activateSimulatorModule && Input.GetButton(STOP_BUTTON))
-            {
-                Application.Quit();
+            Application.Quit();
 #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #endif
-            }
         }
-
-        /// <summary>
-        /// Button für das Beenden des Simulators
-        /// </summary>
-        private const string STOP_BUTTON = "Fire2";
     }
-}
 
-/*! \mainpage
-* Eine mit doxygen erstellte HTML-Dokumentation des Unity-Packages MBVR.
-*
-* Beispiele für die Verwendung der Klassen und Assets finden Sie im Verzeichnis
-* Examples.
- */
+    /// <summary>
+    /// Button für das Beenden des Simulators
+    /// </summary>
+    private const string STOP_BUTTON = "Jump";
+}
